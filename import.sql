945291f6b0b12dcb8e3bde815154d271448d2042
David Salazar <davidksalazar@gmail.com>
2015-04-22 02:38:28 -0500

    add geonameid index to schema.

--- import.sql
+++ import.sql
@@ -82,3 +82,5 @@ INTO TABLE countryinfo
 CHARACTER SET 'UTF8'
 IGNORE 51 LINES
 (iso_alpha2, iso_alpha3, iso_numeric, fips_code, name, capital, areaInSqKm, population, continent, tld, currency, currencyName, phone, postalCodeFormat, postalCodeRegex, languages, geonameid, neighbours, equivalentFipsCode);
+
+delete from geoname where population < 5000;
